4e512c640a3b11273afb9b15659d7a9ca81f4e74
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-05 20:48:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+  "My name is Kish."
+  end
+  
+  get '/hometown' do
+    "My hometown is Sydney."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Da Club by 50 Cent"
+  end
+  
 end
